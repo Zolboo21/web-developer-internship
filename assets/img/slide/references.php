c30596101f141a50580c5ba247eb306920f9e4bb
zolboo21 <zolboo.oz@gmail.com>
2023-02-09 05:48:16 +0900

    Uploading pictures

---
+++ assets/img/slide/references.php
@@ -0,0 +1,13 @@
+<figcaption>
+  Image Title: Total Eclipse of the Moon<br>
+  Source: NASA's Astronomy Picture of the Day (APOD)<br>
+  URL: https://apod.nasa.gov/apod/ap981202.html<br>
+  Copyright: This image is credited to Fred Espenak and is used with permission.
+</figcaption>
+
+<figcaption>
+  Image Title: The Triangulum Galaxy<br>
+  Source: NASA's Astronomy Picture of the Day (APOD)<br>
+  URL: https://apod.nasa.gov/apod/ap151029.html<br>
+  Copyright: This image is credited to Rogelio Bernal Andreo and is used with permission.
+</figcaption>
